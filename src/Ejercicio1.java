ade889568ad3c5dc07ab4556f256bdd5198022df
andresfmj <andresfelipemj468@gmail.com>
2023-08-02 16:23:12 -0500

    primerCommit

---
+++ src/Ejercicio1.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        String inputA = JOptionPane.showInputDialog("Ingresa tu primer valor (debe ser un número entero):");
+        String inputB = JOptionPane.showInputDialog("Ingresa tu segundo valor (debe ser un número entero):");
+
+        try {
+            int numA = Integer.parseInt(inputA);
+            int numB = Integer.parseInt(inputB);
+
+            int sum = numA + numB;
+            int res = numA - numB;
+            int mul = numA * numB;
+            int div = numA / numB;
+
+            JOptionPane.showMessageDialog(null, "La suma de los dos números es: " + sum);
+            JOptionPane.showMessageDialog(null, "La resta de los dos números es: " + res);
+            JOptionPane.showMessageDialog(null, "La multiplicacion de los dos números es: " + mul);
+            JOptionPane.showMessageDialog(null, "La division de los dos números es: " + div);
+        } catch (NumberFormatException e) {
+
+            JOptionPane.showMessageDialog(null, " Ingresa valores enteros válidos.");
+        }
+    }
+}
+
---
+++ src/Ejercicio2.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class Ejercicio2 {
+
+    public static void main(String[] arg){
+        String input = JOptionPane.showInputDialog("Ingresa un valor (debe ser un número entero):");
+
+        try {
+
+            int num = Integer.parseInt(input);
+
+            String result = (num % 2 == 0) ? "es un par" : "es un impar";
+
+            JOptionPane.showMessageDialog(null,"El número " + num + " " + result + ".");
+
+        } catch (NumberFormatException e) {
+
+            JOptionPane.showMessageDialog(null, " Ingresa valores enteros válidos.");
+        }
+    }
+}
